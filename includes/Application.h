03497ba8c1e55612ce1ac2d41d8062312b16a6a1
aneury1 <aneury1@gmail.com>
2024-04-07 21:15:49 +0200

    fix minor

--- includes/Application.h
+++ includes/Application.h
@@ -26,9 +26,11 @@ class Application
     bool dirty;
 
    static Application *applicationObject;
-
+    Application();
+    ~Application();
+ 
    void _internalResize(){
-      ///todo.
+      SDL_SetWindowSize(window, window_rect.w, window_rect.h);
     }
 
     public:
@@ -38,9 +40,6 @@ class Application
             applicationObject = new Application();
         return applicationObject;
     }
-    Application();
-    ~Application();
- 
 
     const bool    IsRunning() const;
     doengine::gfx::Renderer* getRender() const  {return render;}
@@ -75,8 +74,15 @@ class Application
         return window_rect.w;
     }
 
-    typedef unsigned char ColorT;
     void SetWindowPencilColor(doengine::Color color);
+
+    void addState(GameState *state, int id){
+        this->gsm->AddState(state, id);
+    }
+
+    bool setState(int id){
+        gsm->setState(id);
+    }
 
     private:
     void destroy();
