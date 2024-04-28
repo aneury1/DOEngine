8edd88dab2f3de17bde0f1bc6b952a7595540dde
aneury1 <aneury1@gmail.com>
2024-04-28 19:03:24 +0200

    ttf Text start point

--- src/Event/Event.cpp
+++ src/Event/Event.cpp
@@ -64,8 +64,8 @@ void Event::PollEvent()
             doengine::devices::SDLMouse mouse(event.motion.which, mask,
                                               mousePos);
 
-            SDL_Log("SDL_MOUSEMOTION x: %ld,  y:%ld", mousePos.x, mousePos.y);
-            SDL_Log("mousePos Count = %ld", Event::mouseEvent.size());
+            ////SDL_Log("SDL_MOUSEMOTION x: %ld,  y:%ld", mouse.x, mouse.y);
+            ///SDL_Log("Mouse Count = %ld", Event::mouseEvent.size());
 
             for (auto itMouse : Event::mouseEvent)
             {
