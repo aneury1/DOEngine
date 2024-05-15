0a61ac2e4e8ada3ae5474ca6e9704365bd53056d
Aneury Perez <aneury1@gmail.com>
2024-05-15 20:23:27 +0200

    Update includes/StringUtils.h
    Co-authored-by: Victor D. Montero <victordanielmontero@gmail.com>

--- includes/StringUtils.h
+++ includes/StringUtils.h
@@ -9,7 +9,7 @@ void readMapFile(const char *filename, std::vector<std::string>&ret);
 std::vector<std::string> splitString(const std::string& original, const char ch);
 std::vector<char> splitChar(const std::string& original, const std::string& ignoredChars);
 
-std::string vectorOfCharToString(std::vector<char> chars);
+std::string makeString(const std::vector<char>& chars);
 
 int StringAsInt(std::string str);
 
