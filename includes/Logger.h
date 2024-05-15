a4df0564e96e01b3889ac63797c2e08096f476e6
Aneury Perez <aneury1@gmail.com>
2024-05-15 20:22:53 +0200

    Update includes/Logger.h
    Co-authored-by: Victor D. Montero <victordanielmontero@gmail.com>

--- includes/Logger.h
+++ includes/Logger.h
@@ -26,14 +26,6 @@ static const char* TextColorTable[TextColorCounter] =
     "\x1b[35m", // TextColorMagenta
     "\x1b[36m", // TextColorCyan
     "\x1b[37m", // TextColorWhite
-    /*"\x1b[90m", // TEXT_COLOR_BRIGHT_BLACK
-    "\x1b[91m", // TEXT_COLOR_BRIGHT_RED
-    "\x1b[92m", // TEXT_COLOR_BRIGHT_GREEN
-    "\x1b[93m", // TEXT_COLOR_BRIGHT_YELLOW
-    "\x1b[94m", // TEXT_COLOR_BRIGHT_BLUE
-    "\x1b[95m", // TEXT_COLOR_BRIGHT_MAGENTA
-    "\x1b[96m", // TEXT_COLOR_BRIGHT_CYAN
-    "\x1b[97m", // TEXT_COLOR_BRIGHT_WHITE*/
   };
 
   char formatBuffer[8192] = {};
